597e4f1165c6cfa1fdc0555ff0a14a278e312920
668168 <1046701+668168@users.noreply.github.com>
2026-08-15 20:52:29 +0800

    Add HomeScreen Compose MVP

---
+++ app/src/main/java/com/happy/today/ui/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.happy.today.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text(text = "我今天很开心 - 今日记录一件开心的事")
+}
